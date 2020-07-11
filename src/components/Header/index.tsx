9d4c8c1bee49c11dc91ba0667e4e2263741493fe
Laercio Santos <laerciosantosn@gmail.com>
2020-07-11 10:02:04 -0300

    iniciando o projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+  <header className="center">
+    <img
+      src="https://occ-0-3247-1567.1.nflxso.net/dnm/api/v6/TsSRXvDuraoJ7apdkH6tsHhf-ZQ/AAAABe_zvD8XuObbSmtilocAfqrZck14O7O_LKxXx2Rb1wtzgWoPaOtTGzMqrckixb7OFeMQWzY5QXuch4NQxcEdlaRlRewEnYg9XYhA.png?r=295"
+      alt="Logo Breacking Bad"
+    />
+  </header>
+);
+
+export default Header;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container } from './style';
+
+const Home: React.FC = () => (
+  <Container>
+    <Header />
+    <p>Testo Home</p>
+  </Container>
+);
+
+export default Home;
